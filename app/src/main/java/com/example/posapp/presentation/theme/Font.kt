55eda3d46f81e315154b7be355325db45823fb4d
Achmad Afifuddin <achfif@gmail.com>
2026-08-24 05:24:58 +0700

    Add files via upload

--- app/src/main/java/com/example/posapp/presentation/theme/Font.kt
+++ app/src/main/java/com/example/posapp/presentation/theme/Font.kt
@@ -1,8 +1,8 @@
 package com.example.posapp.presentation.theme
 
-import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.googlefonts.Font
 import androidx.compose.ui.text.googlefonts.GoogleFont
 import com.example.posapp.R
 
