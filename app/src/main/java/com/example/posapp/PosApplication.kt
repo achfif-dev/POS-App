b6655175df513c411f7764e999155cffb2ff4c42
Achmad Afifuddin <achfif@gmail.com>
2026-08-24 06:57:41 +0700

    Add files via upload

--- app/src/main/java/com/example/posapp/PosApplication.kt
+++ app/src/main/java/com/example/posapp/PosApplication.kt
@@ -4,4 +4,11 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class PosApplication : Application()
+class PosApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // Dipasang PALING AWAL (sebelum apapun lain di onCreate) supaya menangkap crash yang
+        // terjadi sedini mungkin, termasuk saat inisialisasi dependency Hilt/Room/DataStore.
+        CrashHandler.install(this)
+    }
+}
